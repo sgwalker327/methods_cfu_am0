746a401d07e6565f66336f99e71458c8e381bc3a
Sam Walker <sg.walker327@gmail.com>
2022-11-03 12:38:18 -0600

    Complete define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,27 @@
 
 
 "Hello World".include?("Hello")
-
+# The include method is called on the string object "Hello World"
+# The argument "Hello" is passed, the indlude method is going to ask if the string "Hello" exists in the string object "Hello World."
+# The implicit return value is true
 
 "Hello World".end_with?("Hello")
-
+# The end_with? method is called on the string object "Hello World"
+# The argument "Hello" is passed, the end_with? method is going to ask if the string object "Hello World" ends with "Hello".
+# The implicit return value is false
 
 "Hello World".end_with?("rld")
-
+# The end_with? method is called on the string object "Hello World"
+# The argument "rld" is passed, the end_with? method is going to ask if the string object "Hello World" ends with "rld".
+# The implicit return value is true
 
 12.even?
-
+# The even? method is called on the integer 12.
+# The even? method is asking if the integer is even.
+# The implicit return value is true
 
 18.next
-
+# The next method is called on the integer 18
+# The next method will return the next integer after 18
+# The implicit return value is 19
 
